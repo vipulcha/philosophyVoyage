26391766f629a092fa4db96cdf5119291e74480e
vipulcha <76212979+vipulcha@users.noreply.github.com>
2024-04-27 02:04:50 +0530

    Designing progress and fixed the bug

--- frontend/src/pages/SpecPhil.jsx
+++ frontend/src/pages/SpecPhil.jsx
@@ -3,26 +3,42 @@ import { useParams } from 'react-router-dom';
 
 const SpecPhil = () => {
     const { id } = useParams();
-    const [data,setData] = useState([]);
+    const [data,setData] = useState(null);
 
     const fetchQuotes = async () => {
         const response = await fetch(`http://localhost:5000/philosophers/${id}`);
-        const data = await response.json();
-        setData(data);
-        console.log(data);
+        const dataa = await response.json();
+        setData(dataa);
+        console.log(dataa);
     }
 
     useEffect(() => {
         fetchQuotes();
     },[]);
 
+    
+    if(data === null){
+        return <div>Loading...</div>;
+    }
 
     return (
-        <div className="bg-black text-white h-auto">
+       
+        <div className="bg-black text-white h-auto flex flex-col text-center">
             <div className='text-9xl'>{data.name}</div>
-            <img src={data.photo}/>
+            <div>
+            <img src={data.photo} className='h-64 w-60 content-center'/>
+            </div>
             <div>{data.era}</div>
+            <div>SCHOOLS</div>
             {(data.school).map((sch,key)=>{
+                return (
+                    <div key={key}>
+                        {sch}
+                    </div>
+                )
+            })}
+            <div>IDEAS</div>
+            {(data.ideas).map((sch,key)=>{
                 return (
                     <div key={key}>
                         {sch}
